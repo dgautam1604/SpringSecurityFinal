51930ce8f159c0910f65405d2ac1b302e0d1e985
deepshivam <deepshivamgautam2@gmail.com>
2023-10-12 10:17:49 -0400

    Works for H2 database with sql files to update data

--- src/main/java/com/example/springsecurityfinal/SecurityConfig.java
+++ src/main/java/com/example/springsecurityfinal/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.springsecurityfinal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,16 +8,37 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("deep").password("deep").roles("USER")
-                .and().withUser("deep1").password("deep1").roles("ADMIN");
+//        auth.inMemoryAuthentication()
+//                .withUser("deep").password("deep").roles("USER")
+//                .and().withUser("deep1").password("deep1").roles("ADMIN");
+//        //Pushing the below in DB and using them
+//        auth.jdbcAuthentication().dataSource(dataSource)
+//                .withDefaultSchema()
+//                .withUser(User.withUsername("deep").password("deep").roles("USER"))
+//                .withUser(User.withUsername("deep1").password("deep1").roles("ADMIN"));
+
+        //Fetching datasource the below in DB
+        //Queries are only required in case your column names doesn't match, as username,password,enabled
+        //are required
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select username,password,enabled from users where username=?")
+                .authoritiesByUsernameQuery("select username,authority from authorities where username=?");
+
+
     }
 
     @Override
