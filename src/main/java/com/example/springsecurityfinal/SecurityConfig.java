48048628d425d079a00eecac0d4f8c4507970396
deepshivam <deepshivamgautam2@gmail.com>
2023-10-12 11:11:21 -0400

    Template for JPA before JPA components are added

--- src/main/java/com/example/springsecurityfinal/SecurityConfig.java
+++ src/main/java/com/example/springsecurityfinal/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -18,27 +19,12 @@ import javax.sql.DataSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    DataSource dataSource;
+    UserDetailsService userDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("deep").password("deep").roles("USER")
-//                .and().withUser("deep1").password("deep1").roles("ADMIN");
-//        //Pushing the below in DB and using them
-//        auth.jdbcAuthentication().dataSource(dataSource)
-//                .withDefaultSchema()
-//                .withUser(User.withUsername("deep").password("deep").roles("USER"))
-//                .withUser(User.withUsername("deep1").password("deep1").roles("ADMIN"));
 
-        //Fetching datasource the below in DB
-        //Queries are only required in case your column names doesn't match, as username,password,enabled
-        //are required
-        auth.jdbcAuthentication().dataSource(dataSource)
-                .usersByUsernameQuery("select username,password,enabled from users where username=?")
-                .authoritiesByUsernameQuery("select username,authority from authorities where username=?");
-
-
+        auth.userDetailsService(userDetailsService);
     }
 
     @Override
